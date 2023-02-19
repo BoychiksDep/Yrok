4cd6341f6f92ba6e4f519f709c46d9acd11df8ca
datsiuk7 <datcukbogdan@gmail.com>
2023-02-19 16:06:08 +0200

    lesson5

---
+++ Lesson5/js/script.js
@@ -0,0 +1,26 @@
+// let a = 5
+
+// console.log(a);
+
+let arr = ["12", "12321", "321"];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+let obj = { bill: "значення" }
+
+console.log(obj);
+alert("привіт")
+    // let name = prompt("Повідомлення")
+    // console.log(name);
+    // let conf = confirm("авіав")
+    // console.log(conf);
+    // function fds(fdsf) {
+    //     fdsaf
+    //     retur
+    // }
+
+// console.log(arr);
+// arr.push("fdsfd")
+// console.log(arr);
